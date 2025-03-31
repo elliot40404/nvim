35d988a77300e2be2d4b36c4fecd7f00db6e129a
Elliot <67360013+elliot40404@users.noreply.github.com>
2025-04-01 03:22:32 +0530

    nope

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -21,18 +21,3 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
   desc = 'LSP: Disable hover capability from Ruff',
 })
-
-vim.api.nvim_create_augroup('DashboardAutoOpen', { clear = true })
-vim.api.nvim_create_autocmd({ 'BufDelete' }, {
-  group = 'DashboardAutoOpen',
-  callback = function()
-    vim.schedule(function()
-      local buffers = vim.tbl_filter(function(buf)
-        return vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buflisted and vim.api.nvim_buf_get_name(buf) ~= ''
-      end, vim.api.nvim_list_bufs())
-      if #buffers == 0 then
-        vim.cmd 'lua Snacks.dashboard()'
-      end
-    end)
-  end,
-})
