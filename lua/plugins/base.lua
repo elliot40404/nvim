d299f7bde7c33a7f73ca6ea6b4b877eafd8c366f
Elliot <67360013+elliot40404@users.noreply.github.com>
2025-04-16 13:23:40 +0530

    noice Modified

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -202,7 +202,20 @@ local plugins = {
   {
     'folke/noice.nvim',
     event = 'VeryLazy',
-    opts = {},
+    opts = {
+      lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+        },
+      },
+      presets = {
+        -- bottom_search = true, -- use a classic bottom cmdline for search
+        -- command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+    },
     dependencies = {
       'MunifTanjim/nui.nvim',
     },
@@ -213,6 +226,9 @@ local plugins = {
     cmd = { 'FzfLua' },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {},
+  },
+  {
+    'rcarriga/nvim-notify',
   },
 }
 
